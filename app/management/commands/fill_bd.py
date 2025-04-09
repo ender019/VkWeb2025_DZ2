c6e4a13480f223ec1487326406ba71bb39b06dd1
ender019 <ender-fox@mail.ru>
2025-04-09 19:52:01 +0300

    Изменение заполнения

--- app/management/commands/fill_bd.py
+++ app/management/commands/fill_bd.py
@@ -12,13 +12,14 @@ from random import randint, choices, sample
 class Command(BaseCommand):
     help = 'Fills the database'
     img = ["PELMEN.png", "frog.webp", "volk.jpg", "bird.jpg"]
+    batch = 1000
 
     def add_arguments(self, parser):
         parser.add_argument('ratio', type=int, help='Kol')
 
     def rand_str(self, kol=10) -> str:
         let = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        return "".join(let[randint(0, 51)] for i in range(kol))
+        return "".join(choices(let, k=kol))
     
     def rand_date(self) -> datetime:
         return timezone.make_aware(
@@ -29,28 +30,29 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         kol: int = options['ratio']
+        st_time = datetime.now()
 
-        print("генерация тегов...")
+        print(st_time.now()-st_time, "генерация тегов...")
         tags = [Tag(id=i, title=f"tag{i}") for i in range(kol)]
-        print("генерация юзеров...")
+        print(st_time.now()-st_time, "генерация юзеров...")
         users = [User(
                 username=self.rand_str(randint(10,50)),
                 email=self.rand_str(randint(5,20))+"@mail.ru"
             ) for i in range(kol)]
-        print("генерация профилей...")
+        print(st_time.now()-st_time, "генерация профилей...")
         profiles = [Profile(
             nickname=self.rand_str(randint(10,50)),
             avatar=f"/img/{choices(self.img)[0]}",
             user=users[i]
         ) for i in range(kol)]
-        print("генерация вопросов...")
+        print(st_time.now()-st_time, "генерация вопросов...")
         questions = [Question(
                 title=f"Question {i}",
                 text="question "*randint(1, 100),
                 posted=self.rand_date(),
                 profile=profiles[randint(0, kol-1)]
             ) for i in range(kol*10)]
-        print("генерация ответов...")
+        print(st_time.now()-st_time, "генерация ответов...")
         answers = [Answer(
                 text="answer "*randint(1, 10)+str(i),
                 correct=randint(0, 1),
@@ -59,28 +61,40 @@ class Command(BaseCommand):
                 profile=profiles[randint(0, kol-1)]
             ) for i in range(kol*100)]
 
-        print("запись объектов...")
+        print(st_time.now()-st_time, "запись объектов...")
         Tag.objects.bulk_create(tags)
         User.objects.bulk_create(users)
         Profile.objects.bulk_create(profiles)
         Question.objects.bulk_create(questions)
         Answer.objects.bulk_create(answers)
 
-        print("генерация связей тегов...")
-        QuestionsTags.objects.bulk_create([
-            QuestionsTags(question=q, tag=t)
-            for q in questions
-            for t in sample(tags, k=randint(1, kol))
-        ])
-        print("генерация связей вопросов...")
-        QuestionsLikes.objects.bulk_create([
-            QuestionsLikes(question=q, profile=p)
-            for q in questions
-            for p in sample(profiles, k=randint(0, kol))
-        ])
-        print("генерация связей ответов...")
-        AnswersLikes.objects.bulk_create([
-            AnswersLikes(answer=a, profile=p)
-            for a in answers
-            for p in sample(profiles, k=randint(0, kol))
-        ])
+        print(st_time.now()-st_time, "генерация связей тегов...")
+        ind = 0
+        while ind < kol*10:
+            QuestionsTags.objects.bulk_create([
+                QuestionsTags(question=questions[i], tag=t)
+                for i in range(ind, min(ind + self.batch, kol*10))
+                for t in sample(tags, k=randint(1, 20))
+            ])
+            ind += self.batch
+            print(f"    Готово {ind} записей из {kol*10}")
+        print(st_time.now()-st_time, "генерация связей вопросов...")
+        ind = 0
+        while ind < kol*10:
+            QuestionsLikes.objects.bulk_create([
+                QuestionsLikes(question=questions[i], profile=p)
+                for i in range(ind, min(ind + self.batch, kol*10))
+                for p in sample(profiles, k=randint(0, kol))
+            ])
+            ind += self.batch
+            print(f"    Готово {ind} записей из {kol*10}")
+        print(st_time.now()-st_time, "генерация связей ответов...")
+        ind = 0
+        while ind < kol*100:
+            AnswersLikes.objects.bulk_create([
+                AnswersLikes(answer=answers[i], profile=p)
+                for i in range(ind, min(ind + self.batch, kol * 100))
+                for p in sample(profiles, k=randint(0, kol))
+            ])
+            ind += self.batch
+            print(f"    Готово {ind} записей из {kol*10}")
